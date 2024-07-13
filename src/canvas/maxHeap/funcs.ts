e1e24b9a6477ed734cae7080b653b69075682265
Chetan Satpute <chetan.satpute2002@gmail.com>
2024-07-13 22:21:19 +0530

    Max heap: set

--- src/canvas/maxHeap/funcs.ts
+++ src/canvas/maxHeap/funcs.ts
@@ -1,8 +1,14 @@
-import {AlgorithmFunctionType, FunctionSection} from '../types.js';
+import {
+  AlgorithmFunctionType,
+  FunctionArgumentType,
+  FunctionSection,
+} from '../types.js';
 import randomMaxHeap from './modify/random.js';
+import setMaxHeap from './modify/setHeap.js';
 
 export enum MaxHeapFunctionId {
   Random = 'random',
+  Set = 'set',
 }
 
 export const maxHeapFunctionSections: FunctionSection[] = [
@@ -15,12 +21,27 @@ export const maxHeapFunctionSections: FunctionSection[] = [
         parameters: [],
         animated: false,
       },
+      {
+        id: MaxHeapFunctionId.Set,
+        name: 'Set max heap',
+        parameters: [
+          {
+            label: 'values',
+            placeholder: '1,2,3,4,5',
+            argumentType: FunctionArgumentType.NumberArray,
+            supportingText:
+              'comma seperated values will be inserted in sequence',
+          },
+        ],
+        animated: false,
+      },
     ],
   },
 ];
 
 export const maxHeapFunctionMap: Record<string, AlgorithmFunctionType> = {
   [MaxHeapFunctionId.Random]: randomMaxHeap,
+  [MaxHeapFunctionId.Set]: setMaxHeap,
 };
 
 export const maxHeapCodeMap: Record<string, string> = {};
